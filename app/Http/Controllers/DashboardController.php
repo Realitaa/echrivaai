2cc77ff418a1ebcf770abaecfee2430576be5870
Realitaa <rezamuliaputra927@gmail.com>
2026-04-15 18:45:49 +0700

    feat: implement backend user management module and dashboard controller with route integration

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    // Dashboard will show application statistic later, for now, rendering the pages is enough
+    // public function index()
+    // {
+    //     // Processing some data here
+    //     return Inertia::render('Dashboard');
+    // }
+}
---
+++ resources/js/pages/admin/Users.vue
@@ -0,0 +1,3 @@
+<template>
+  This is Users page which listing all user, approving incoming registering user and doing user management.
+</template>
